80d4686b093c8f7bcf7d02f811b02fff1fbbbc8c
mela04-debug <vanemela04@gmail.com>
2026-05-12 22:37:49 -0400

    avance módulo proyectos

--- src/dao/MaterialDAO.java
+++ src/dao/MaterialDAO.java
@@ -8,6 +8,6 @@ package dao;
  *
  * @author Melani
  */
-public class UsuarioDAO {
+public class MaterialDAO {
     
 }
--- src/modelo/FaseProyecto.java
+++ src/modelo/FaseProyecto.java
@@ -1,13 +1,14 @@
+package modelo;
+
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package dao;
 
 /**
  *
  * @author Melani
  */
-public class UsuarioDAO {
+public class FaseProyecto {
     
 }
