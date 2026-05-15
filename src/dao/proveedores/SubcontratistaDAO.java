41559d77341cbee8461cbc562cb67c0971c0d683
mela04-debug <vanemela04@gmail.com>
2026-05-14 21:16:41 -0400

    Guardando mis cambios

---
+++ src/dao/proveedores/SubcontratistaDAO.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package dao.proveedores;
+
+/**
+ *
+ * @author Melani
+ */
+public class SubcontratistaDAO {
+    
+}
